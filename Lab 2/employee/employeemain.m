096ef2be509d9b08e5f38627d26b311805e25dd7
adityanshastry <adityashastry1992@gmail.com>
2013-08-31 19:09:39 +0530

    Update employeemain.m

--- Lab 2/employee/employeemain.m
+++ Lab 2/employee/employeemain.m
@@ -2,27 +2,16 @@
 #import "Employee.m"
 #include<stdio.h>
 
-int main(int argc, const char* argv[]){
-
-//NSString *name1,*dept1;
-float num;
-Employee *myEmp = [[Employee alloc] init];
-/*
-NSLog(@"\nEnter name:");
-scanf("%s",&name1);
-NSLog(@"\nEnter dept:");
-scanf("%s",&dept1);
-*/
-//NSLog(@"\nEnter id:");
-//scanf("%d",&num);
-num = 10.345f;
-[myEmp setEmpId:num];
-NSLog(@"Employee details:%d",[myEmp empId]);
-[myEmp setName:@"akshay"];
-
-[myEmp setDept:@"ise"];
-
-NSLog(@"Employee details:%@ %@",[myEmp name],[myEmp dept]); 
-[myEmp release];
-
-return 0;}
+int main(int argc, const char* argv[])
+{
+  int num;
+  Employee *myEmp = [[Employee alloc] init]; //allocate memory and initialize
+  num = 123;
+  [myEmp setEmpId:num];
+  NSLog(@"Employee details:%d",[myEmp empId]);
+  [myEmp setName:@"Aditya N Shastry"];
+  [myEmp setDept:@"ISE"];
+  NSLog(@"Employee details:%@ %@",[myEmp name],[myEmp dept]); 
+  [myEmp release]; //release memory
+  return 0;
+}
