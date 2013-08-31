eda02db7de86d511772552e88f02b75d51188c83
adityanshastry <adityashastry1992@gmail.com>
2013-08-31 19:00:31 +0530

    Update hellomain.m

--- Lab 1 - Hello World/hellomain.m
+++ Lab 1 - Hello World/hellomain.m
@@ -1,11 +1,13 @@
+//main file for hello world class
+
 #import<Foundation/Foundation.h>
 #import "hello.h"
 
 int main(int argc,const char *argv[])
 {
-hello *hell=[[hello alloc]init];
-[hell addStringValue:"Hello!"];
-[hell print];
-[hell release];
+hello *h=[[hello alloc]init]; //create and initialize an object
+[h addStringValue:"Hello!"]; //send input string
+[h print]; //print input string
+[h release]; //release the objects memory
 return 0;
 }
