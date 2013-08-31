21d2bb2bd1b2945ef2f389ca199651f08e0bc3eb
adityanshastry <adityashastry1992@gmail.com>
2013-08-31 19:12:46 +0530

    Update and rename employeemain.m to main.m

---
+++ Lab 2/employee prop n synth/main.m
@@ -0,0 +1,19 @@
+//main code remains same as the one with classes where getter and setter methods are defined by the programmer
+#import<Foundation/Foundation.h>
+#import "Employee.m"
+#include<stdio.h>
+
+int main(int argc, const char* argv[])
+{
+  int num;
+  id myEmp = [[Employee alloc] init];
+  NSLog(@"\nEnter id:");
+  scanf("%d",&num);
+  [myEmp setEmpId:num];
+  NSLog(@"Employee id:%d",[myEmp empId]);
+  [myEmp setName:@"abraham"];
+  [myEmp setDept:@"bt"];
+  NSLog(@"Employee name:%@ Employee dept: %@",[myEmp name],[myEmp dept]); 
+  [myEmp release];
+  return 0;
+}
