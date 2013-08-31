2c50c4ac5b89e9bc95fa1bbfb6e2e1218ffbc76b
adityanshastry <adityashastry1992@gmail.com>
2013-08-31 19:04:27 +0530

    Update main.m

--- Lab 2/Calculator/main.m
+++ Lab 2/Calculator/main.m
@@ -5,14 +5,14 @@
 int main(int argc, char *argv[])
 {
 	double num1, num2;
-	Calculator *calc = [[Calculator alloc]init];
+	Calculator *calc = [[Calculator alloc]init]; //allocate memory and initialize the object
 	NSLog(@"\nEnter 2 numbers:\n");
-	scanf("%lf%lf", &num1, &num2);
-	//printf("abc");
+	scanf("%lf%lf", &num1, &num2); //accept numbers from user
+	//initiaize the attributes 
 	[calc setNum1:num1];
 	[calc setNum2:num2];
-	//printf("%lf,%lf", num1, num2);
-
+	
+	//perform addition, subtraction, multiplication and division and print respective values
 	NSLog(@"%.4lf + %.4lf = %lf", num1, num2, [calc add]);
 	NSLog(@"%.4lf - %.4lf = %lf", num1, num2, [calc sub]);
 	NSLog(@"%.4lf * %.4lf = %lf", num1, num2, [calc mul]);
